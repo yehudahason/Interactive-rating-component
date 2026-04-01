bdda2e0550d00ca9cef5a5b75a311faf19903169
Yehuda Hason <yehudaxt@gmail.com>
2026-04-01 17:51:08 +0300

    star fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 const buttons = document.querySelectorAll(".rating-button");
+const stars = document.querySelectorAll(".icon-container");
 const selectedRating = document.querySelector(".selected-rating");
 const submitButton = document.querySelector(".submit-button");
 const ratingContainer = document.querySelector(".rating-state");
 const thankYouContainer = document.querySelector(".thank-you-state");
 
-let ratingValue = null;
+let ratingValue = 1;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+    for (let i = 0; i < buttons.length; i++) {
+      if (i <= button.value - 1) {
+        stars[i].classList.add("active");
+      } else {
+        stars[i].classList.remove("active");
+      }
+    }
     buttons.forEach((btn) => btn.classList.remove("active"));
     button.classList.add("active");
     ratingValue = button.value;
